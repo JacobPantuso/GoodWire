0ecb00667bb4ec4fcc7403923084be24acb00c6b
Jacob Pantuso <jacobpantuso@outlook.com>
2023-03-19 20:44:43 -0400

    Payment form finalized

---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+function myFunction(element) {
+    document.getElementById(element).classList.toggle("show");
+    // change the arrow to fa-solid fa-caret-up where the icon has id of element-caret
+    var caret = document.getElementById(element + "-caret");
+    if (caret.classList.contains('fa-caret-down')) {
+        caret.classList.remove('fa-caret-down');
+        caret.classList.add('fa-caret-left');
+    } else {
+        caret.classList.remove('fa-caret-left');
+        caret.classList.add('fa-caret-down');
+    }
+    var products = document.getElementById("products");
+    if (products.classList.contains('show') && element != "products") {
+        products.classList.remove('show');
+    }
+    var brands = document.getElementById("brands");
+    if (brands.classList.contains('show') && element != "brands") {
+        brands.classList.remove('show');
+    }
+    var services = document.getElementById("services");
+    if (services.classList.contains('show') && element != "services") {
+        services.classList.remove('show');
+    }
+    var support = document.getElementById("support");
+    if (support.classList.contains('show') && element != "support") {
+        support.classList.remove('show');
+    }
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var products = document.getElementById("products");
+        var productsCaret = document.getElementById("products-caret");
+        if (products.classList.contains('show')) {
+            products.classList.remove('show');
+            productsCaret.classList.remove('fa-caret-left');
+            productsCaret.classList.add('fa-caret-down');
+        }
+        var brands = document.getElementById("brands");
+        var brandsCaret = document.getElementById("brands-caret");
+        if (brands.classList.contains('show')) {
+            brands.classList.remove('show');
+            brandsCaret.classList.remove('fa-caret-left');
+            brandsCaret.classList.add('fa-caret-down');
+        }
+        var services = document.getElementById("services");
+        var servicesCaret = document.getElementById("services-caret");
+        if (services.classList.contains('show')) {
+            services.classList.remove('show');
+            servicesCaret.classList.remove('fa-caret-left');
+            servicesCaret.classList.add('fa-caret-down');
+        }
+        var support = document.getElementById("support");
+        var supportCaret = document.getElementById("support-caret");
+        if (support.classList.contains('show')) {
+            support.classList.remove('show');
+            supportCaret.classList.remove('fa-caret-left');
+            supportCaret.classList.add('fa-caret-down');
+        }
+    }
+  }
--- src/js/payment.js
+++ src/js/payment.js
@@ -10,3 +10,30 @@ function processPayment()
     }
     , 4000);
 }
+
+function validatePayment() {
+    var name = document.getElementById('name').value;
+    var card = document.getElementById('card').value;
+    var cvv = document.getElementById('cvv').value;
+    let currentDate = new Date();
+    let expiryDate = document.getElementById("date").value;
+    let expiryMonth = expiryDate.split('/')[0];
+    let expiryYear = expiryDate.split('/')[1];
+    let expiryDateObj = new Date(`20${expiryYear}`, expiryMonth - 1, 1);
+    if (name == '' || card.length != 16 || cvv.length != 3 || expiryDateObj < currentDate) {
+        if (name == '') {
+            document.getElementById('name').style.borderColor = 'red';
+        } 
+        if (card.length != 16) {
+            document.getElementById('card').style.borderColor = 'red';
+        }
+        if (cvv.length != 3) {
+            document.getElementById('cvv').style.borderColor = 'red';
+        } 
+        if (expiryDateObj < currentDate || expiryDate == '') {
+            document.getElementById('date').style.borderColor = 'red';
+        }
+    } else {
+        window.location.href = 'process-payment.html';
+    }
+}
