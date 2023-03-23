4fbee2f89e4b462b22e718a635e2b71d282260cb
Mohamad <m.elharoufi@hotmail.com>
2023-03-23 18:30:42 -0400

    prod_class

--- src/js/product.js
+++ src/js/product.js
@@ -1,3 +1,71 @@
+var products = [];
 export class Product {
+    constructor(name, id, category, price, description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.id = id;
+        this.category = category;
+      }
+
+      getName() {
+        return this.name;
+      }
+
+      getPrice() {
+        return this.price;
+      }
+
+      getDescription() {
+        return this.description;
+      }
+
+      getId() {
+        return this.id;
+      }
+
+      getCategory() {
+        return this.category;
+      }
     
+      setName(name) {
+        this.name = name;
+      }
+    
+      setPrice(price) {
+        this.price = price;
+      }
+
+      setDescription(description) {
+        this.description = description;
+      }
+
+      setId(id) {
+        this.id = id;
+      }
+
+      setCategory(category){
+        this.category = this.category;
+      }
+
+
+    }
+
+export function updateStorage() {
+    localStorage.setItem('products', JSON.stringify(products));
+} 
+
+export function receiveStorage() {
+    var retrievedProducts = JSON.parse(localStorage.getItem('products'));
+    for (var i = 0; i < retrievedProducts.length; i++) {
+        new Product(retrievedProducts[i].name, retrievedProducts[i].price, retrievedProducts[i].description)
+    }
+}
+
+export function retrieveObject(id) {
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].id == id) {
+            return products[i];
+        }
+    }
 }
