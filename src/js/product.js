c025deb31009998409e5808b25ce7c6174a23d36
Mohamad <m.elharoufi@hotmail.com>
2023-03-23 21:24:04 -0400

    updated

--- src/js/product.js
+++ src/js/product.js
@@ -70,3 +70,20 @@ export function retrieveObject(id) {
         }
     }
 }
+
+const button = document.querySelector(".addtocart");
+const done = document.querySelector(".done");
+console.log(button);
+let added = false;
+button.addEventListener('click',()=>{
+  alert("hello")
+  if(added){
+    done.style.transform = "translate(-110%) skew(-40deg)";
+    added = false;
+  }
+  else{
+    done.style.transform = "translate(0px)";
+    added = true;
+  }
+    
+});
