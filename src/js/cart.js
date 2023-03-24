d4a6b81ebee446a3519a7d4d77d57581b1de5b88
farah Adi <farahadi32@gmail.com>
2023-03-24 00:25:33 -0400

    cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,5 +1,7 @@
-console.log('hello')
-export class Cart {
+let carts = document.querySelectorAll('.fas fa-cart-plus'); 
 
-
+for (let i = 0; i < carts.length; i++) {
+   carts[i].addEventListener('click', () => {
+    console.log("add to cart"); 
+   })
 }
